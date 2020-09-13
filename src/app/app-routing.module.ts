8cb0849c7da13804d9acba8e74df4eaaf7af5540
unknown <hajaregauravg@gmail.com>
2020-09-13 16:14:26 +0530

    login component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 /* Components*/
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent}
+  { path: '', component: HomeComponent},
+  {
+    path: 'login',
+    component: LoginComponent
+  }
 ];
 
 @NgModule({
