0d6475aa8dcd85535cc41f9a1e37fe02e77ddd7e
unknown <hajaregauravg@gmail.com>
2020-10-02 20:50:25 +0530

    registeration page created and linked

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { Routes, RouterModule } from '@angular/router';
 /* Components*/
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ];
 
